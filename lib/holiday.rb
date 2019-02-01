d0b1ab8ef2738c9bdec954e4405a7871303db05c
Christoph Gruener <christophgruener@me.com>
2019-02-01 16:59:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -120,10 +120,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   bbq_days = []
   holiday_hash.each do |season|
-    season.each do |seasons, days|
-      days.each do |key, value| 
-        if value.include?("BBQ")
-          bbq_days.push(days.keys)
+    
         end
       end
     end
